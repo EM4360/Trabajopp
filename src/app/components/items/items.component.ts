82b1e9ce3c155b9b9c1cb744bf095ceebadf3bd9
“EM4360” <emapatimomo13@gmail.com>
2023-06-30 17:54:28 -0300

    creamos la carpeta models con un items.ts para declarar nuestro objetos y exportamos el items en items.component.ts

--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
+import {item} from "../../models/item"
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
   styleUrls: ['./items.component.css']
 })
-export class ItemsComponent {
+export class ItemsComponent{
+items : item[] = [
+  {
+    id: 0,
+    title: "manzana",
+    price: 10.5,
+    quantity:4,
+    complete: false
+   },
+   {
+    id: 1,
+    title: "pan",
+    price: 3.5,
+    quantity:8,
+    complete: false
+   }
+];
 
 }
+
+
---
+++ src/app/models/item.ts
@@ -0,0 +1,7 @@
+export class item{
+    id: number = 0;
+    title: string = "";
+    price: number = 0;
+    quantity: number = 0;
+    complete: boolean = false;
+}
